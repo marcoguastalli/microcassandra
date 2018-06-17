a5062e19f3878bd290e51ce3e86dfd8339a064d4
Marco Guastalli <marco.guastalli@netcentric.biz>
2018-06-17 11:32:32 +0200

    first commit

---
+++ src/test/java/net/marco27/apps/bookmarks/BookmarksApplicationTest.java
@@ -0,0 +1,15 @@
+package net.marco27.apps.bookmarks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookmarksApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
